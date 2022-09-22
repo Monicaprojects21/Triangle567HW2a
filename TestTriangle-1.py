b9955c346d66480d53ccf5e35ab6a5df6dc14a51
Monicaprojects21 <monicamallampatti21@gmail.com>
2022-09-21 22:55:18 -0400

    test commit

--- TestTriangle-1.py
+++ TestTriangle-1.py
@@ -18,76 +18,78 @@ class TestTriangles(unittest.TestCase):
     # define multiple sets of tests as functions with names that begin
 
     def testEquilateralTriangle1(self):
-        self.assertEqual(classifyTriangle(2, 2, 2), 'Equilateral')
+            self.assertEqual(classifyTriangle(2, 2, 2), 'Equilateral')
 
     def testEquilateralTriangle2(self):
         self.assertEqual(classifyTriangle(15, 15, 15), 'Equilateral')
 
     def testEquilateralTriangle3(self):
-        self.assertNotEqual(classifyTriangle(10, 1, 10), 'Equilateral')
+        self.assertEqual(classifyTriangle(10, 10, 10), 'Equilateral')
 
     # Testing Isosceles Triangles
-    def testIsoscelesTrianglezzzz(self):
-        self.assertEqual(classifyTriangle(5, 5, 3), 'Isosceles')
+    def testIsoscelesTriangle1(self):
+        self.assertEqual(classifyTriangle(5, 5, 3), 'Isoceles')
 
-    def testIsoscelesTrianglezzz(self):
-        self.assertEqual(classifyTriangle(4, 6, 6), 'Isosceles')
+    def testIsoscelesTriangle2(self):
+        self.assertEqual(classifyTriangle(4, 6, 6), 'Isoceles')
 
-    def testIsoscelesTrianglezz(self):
-        self.assertEqual(classifyTriangle(8, 5, 8), 'Isosceles')
+    def testIsoscelesTriangle3(self):
+        self.assertEqual(classifyTriangle(8, 5, 8), 'Isoceles')
 
-    def testIsoscelesTrianglez(self):
-        self.assertEqual(classifyTriangle(6, 6, 4), 'Isosceles')
+    def testIsoscelesTriangle4(self):
+        self.assertEqual(classifyTriangle(6, 6, 4), 'Isoceles')
 
     # Testing Scalene Triangles
-    def testScaleneTriangleyyyy(self):
-        self.assertEqual(classifyTriangle(10, 11, 12), 'Scalene')
+    def testScaleneTriangle1(self):
+        self.assertEqual(classifyTriangle(10, 5, 12), 'Scalene')
 
-    def testScaleneTriangleyyy(self):
+    def testScaleneTriangle2(self):
         self.assertEqual(classifyTriangle(4, 2, 3), 'Scalene')
 
-    def testScaleneTriangleyy(self):
+    def testScaleneTriangle3(self):
         self.assertEqual(classifyTriangle(100, 110, 112), 'Scalene')
 
-    def testScaleneTriangley(self):
+    def testScaleneTriangle4(self):
         self.assertNotEqual(classifyTriangle(10, 10, 12), 'Scalene')
 
     # Testing Invalid Inputs
-    def testInvalidInputt(self):
+    def testInvalidInput1(self):
         self.assertEqual(classifyTriangle(-1, -1, -1), 'InvalidInput')
 
-    def testInvalidInputh(self):
+    def testInvalidInput3(self):
         self.assertEqual(classifyTriangle("200", "0", "0"), 'InvalidInput')
+    
+    def testInvalidInput4(self):
+        self.assertEqual(classifyTriangle("a", "0", "0"), 'InvalidInput')
 
     # Testing Not a Triangle
-    def testNotATriangle8(self):
+    def testNotATriangle1(self):
         self.assertEqual(classifyTriangle(5, 1, 1), 'NotATriangle')
 
-    def testNotATriangle5(self):
+    def testNotATriangle2(self):
         self.assertEqual(classifyTriangle(1, 5, 1), 'NotATriangle')
 
-    def testNotATriangle6(self):
+    def testNotATriangle3(self):
         self.assertEqual(classifyTriangle(1, 1, 5), 'NotATriangle')
 
-    def testNotATriangle7(self):
+    def testNotATriangle4(self):
         self.assertEqual(classifyTriangle(1, 17, 5), 'NotATriangle')
 
     # Testing Right Triangles
-    def testRightTriangledd(self):
+    def testRightTriangle1(self):
         self.assertEqual(classifyTriangle(3, 4, 5), 'Right')
 
-    def testRightTriangled(self):
+    def testRightTriangle2(self):
         self.assertEqual(classifyTriangle(5, 3, 4), 'Right')
 
-    def testRightTriangless(self):
+    def testRightTriangle3(self):
         self.assertEqual(classifyTriangle(13, 12, 5), 'Right')
 
-    def testRightTrianglesss(self):
+    def testRightTriangle4(self):
         self.assertEqual(classifyTriangle(8, 6, 10), 'Right')
 
-    def testRightTrianglessss(self):
+    def testRightTriangle5(self):
         self.assertNotEqual(classifyTriangle(21, 6, 10), 'Right')
-
 
 if __name__ == '__main__':
     print('Running unit tests')
